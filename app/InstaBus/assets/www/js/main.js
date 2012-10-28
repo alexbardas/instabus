7eb6472671187d11dc68bd39e8e1041dbbe3abc5
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2012-10-28 06:04:25 +0200

    work on a list of nearby stations

--- app/InstaBus/assets/www/js/main.js
+++ app/InstaBus/assets/www/js/main.js
@@ -3,6 +3,13 @@
         window.InstaBus = {};
     }
 
+    var $landing = $('#landing'),
+        $map = $('#map'),
+        $pickStations = $('#pick-stations'),
+        $transports = $('#transports');
+
+    var currentStation;
+
     var initMap = function () {
         var headerHeight = $('#map [data-role="header"]').height(),
             footerHeight = $('#map [data-role="footer"]').height(),
@@ -17,6 +24,24 @@
         window.InstaBus.initMap();
     };
 
+    var onBeforeChange = function () {
+        if (location.hash === '#pick-station') {
+            populateNearbyStations();
+        }
+    }
+    var populateNearbyStations = function (event) {
+        var mapCenter = window.InstaBus.map.getCenter();
+        var closestStations = Utils.getClosestStations(new Point(c.lat, c.lng), InstaBus.stations, 10);
+        var html = '<ul data-role="listview" data-filter="true">';
+        for (var i = 0, n = closestStations.lenght; i < n; i ++) {
+            var station = closestStations[i];
+            html += '<li><a href="#map">'+station.nume+'</a></li>';
+        }
+        $pickStations.find('ul').replaceWith($(closestStations)).listview();
+    };
+
     // on page loaded
     $('#map').one('pageshow', initMap);
+    //$(document).live('pagecreate', onBeforeChange);
+
 })();
